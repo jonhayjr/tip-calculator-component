41d2e2209dd3b2d357a2782faa47f951a8b6a60f
jonhayjr <jonhayjr@gmail.com>
2021-09-13 18:18:00 -0700

    Refactored JS logic and created objects to stored values

--- js/app.js
+++ js/app.js
@@ -1,9 +1,50 @@
 $( document ).ready(() => {
 
-    //Initialize bill and tip percent variables
-    let bill = 0;
-    let tipPercent = 0;
+    //Object to store tip splitter values and methods
+    const tipSplitter = {
+        previousBillAmount: 0,
+        previousTipPercent: 0,
+        previousPersonCount: 0,
+        billAmount: 0,
+        tipPercent: 0,
+        tipAmount: 0,
+        personCount: 0,
+        personBillAmount: 0,
+        personTipAmount: 0,
+        personTotalAmount: 0,
+        getTipAmount() {
+            this.tipAmount = this.tipPercent * this.billAmount;
+        },
+        getPersonBillAmount() {
+            this.personBillAmount = this.billAmount / this.personCount;
+        },
+        getPersonTipAmount() {
+            this.personTipAmount = parseFloat(this.tipAmount / this.personCount);
+        },
+        getPersonTotalAmount() {
+            this.personTotalAmount = parseFloat(this.personTipAmount + this.personBillAmount);
+        },
+        recalcPersonValues() {
+            //Get tip amount based on bill and tipPercent
+            this.getTipAmount();
 
+            //Get bill amount per a person based on person count value;
+            this.getPersonBillAmount();
+
+            //personTipAmount is the tipAmount divided by the personCount
+            this.getPersonTipAmount();
+
+            //personTotalAmount is the individual bill plus the individual tip
+            this.getPersonTotalAmount();
+
+            //Update tip amount per a person text.
+            $('.tip-amount-section__value').text(`$${this.personTipAmount.toFixed(2)}`);
+                
+            //Updates total amount per a person text.
+            $('.person-total-section__value').text(`$${this.personTotalAmount.toFixed(2)}`);
+        }
+    }
+ 
     //On bill input change, update bill variable value  
     $('#bill').on('change', () => {
         //Store bill input value as integer
@@ -24,8 +65,17 @@ $( document ).ready(() => {
             //Hide validation error
             $('#bill-validation-error').css('display', 'none');
 
+            //Set previousBillAmount equal to current bill amount value
+            tipSplitter.previousBillAmount = tipSplitter.billAmount;
+
            //Sets bill variable to input value
-           bill = billValue;
+           tipSplitter.billAmount = billValue;
+
+            //If bill amount changes after other fields are populated, recalculate other values
+            if (tipSplitter.billAmount !== tipSplitter.previousBillAmount && tipSplitter.tipPercent !== 0 && tipSplitter.personCount !== 0) {
+                tipSplitter.recalcPersonValues();
+            }
+        
 
            //Adds decimal to input value if it doesn't have one
            if (billValue % 1 === 0) {
@@ -36,8 +86,16 @@ $( document ).ready(() => {
     })
     //On tip button click, update tip variable value
     $('.tip-button').on('click', (e) => {
-      //Gets tip percent amount from variable that was clicked and updates tipPercent variable
-      tipPercent = parseFloat(e.target.attributes[1].value);
+      //Sets previous tip percent equal to current tip percent
+      tipSplitter.previousTipPercent = tipSplitter.tipPercent;
+
+      //Gets tip percent percent from variable that was clicked and updates tipPercent variable
+      tipSplitter.tipPercent = parseFloat(e.target.attributes[1].value);
+
+      //If tip percent changes and other fields are populated, recalculate the other fields.
+      if (tipSplitter.tipPercent !== tipSplitter.previousTipPercent && tipSplitter.billAmount !== 0 && tipSplitter.personCount !== 0) {
+        tipSplitter.recalcPersonValues();
+      }
     });
 
     //On tip input change, update tip variable with value 
@@ -47,45 +105,40 @@ $( document ).ready(() => {
 
         //Checks if tipValue is a number
         if (!isNaN(tipValue)) {
+            //Sets previous tip percent equal to current tip percent
+            tipSplitter.previousTipPercent = tipSplitter.tipPercent;
+
             //Value divided by 100 is stored in tipPercent variable
-            tipPercent = tipValue / 100;
+            tipSplitter.tipPercent = tipValue / 100;
+
+            //If tip percent changes and other fields are populated, recalculate the other fields.
+            if (tipSplitter.tipPercent !== tipSplitter.previousTipPercent && tipSplitter.billAmount !== 0 && tipSplitter.personCount !== 0) {
+                tipSplitter.recalcPersonValues();
+            }
         }
     })
 
     //Handle change to person count input field
     $('#person-count').on('change', () => {
+        //Sets previous person count equal to current person count
+        tipSplitter.previousPersonCount = tipSplitter.personCount;
+
         //Store person count input value in variable
-        const personCount = parseInt($('#person-count').val());
-        
+        tipSplitter.personCount = parseInt($('#person-count').val());
+      
         //Check to see if person count is greater than 0
-        if (personCount > 0) {
+        if (tipSplitter.personCount > 0) {
             //Remove validation error class
             $('#person-count').removeClass('validation-error');
 
             //Hide validation error
             $('#person-count-validation-error').css('display', 'none');
 
-            //Get tip amount based on bill and tipPercent
-            const tipAmount = bill * tipPercent;
-
-            //Get bill amount per a person based on person count value;
-            const personBillAmount = bill / personCount;
-
-
-            //Check if tipAmount is greater than 0.
-            if (tipAmount > 0) {
-                //personTipAmount is the tipAmount divided by the personCount
-                const personTipAmount = parseFloat(tipAmount / personCount);
-                //personTotalAmount is the individual bill plus the individual tip
-                const personTotalAmount = parseFloat(personTipAmount + personBillAmount);
-
-                //Update tip amount per a person text.
-                $('.tip-amount-section__value').text(`$${personTipAmount.toFixed(2)}`);
-                
-                //Updates total amount per a person text.
-                $('.person-total-section__value').text(`$${personTotalAmount.toFixed(2)}`);
-    
+            //If person count changed and other values are present, recalculate person values
+            if (tipSplitter.personCount !== tipSplitter.previousPersonCount  && tipSplitter.billAmount !== 0 && tipSplitter.tipPercent !== 0) {
+                tipSplitter.recalcPersonValues();
             }
+    
         } else {
             //If person count is less than 0️
 
@@ -98,9 +151,17 @@ $( document ).ready(() => {
     })
 
     $('.btn-reset').on('click', () => {
-        //Set bill and tipPercent variables to 0
-        bill = 0;
-        tipPercent = 0;
+        //Set object variables to 0
+        tipSplitter.previousBillAmount = 0;
+        tipSplitter.previousTipPercent = 0;
+        tipSplitter.previousPersonCount = 0;
+        tipSplitter.billAmount = 0;
+        tipSplitter.tipPercent = 0;
+        tipSplitter.tipAmount = 0;
+        tipSplitter.personCount = 0;
+        tipSplitter.personBillAmount = 0;
+        tipSplitter.personTipAmount = 0;
+        tipSplitter.personTotalAmount = 0;
 
         //Set bill input value to blank
         $('#bill').val('');
